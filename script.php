b663701d67eb92cc99165c58a4e5968fd80cf546
Sergey Tolkachyov <musstudent@mail.ru>
2023-05-19 21:16:37 +0400

    remove unused namespaces

--- script.php
+++ script.php
@@ -3,10 +3,6 @@
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Installer\Installer;
-use Joomla\CMS\Installer\InstallerHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Version;
 
 /**
